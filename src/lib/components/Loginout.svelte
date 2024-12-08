4ea9b034670012266878d9d16bfa031ea5199c82
Lukas Rosenthaler <lukas.rosenthaler@unibas.ch>
2024-12-09 00:30:10 +0100

    Ongoing bootstrap of APP

--- src/lib/components/Loginout.svelte
+++ src/lib/components/Loginout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	let apiUrl = import.meta.env.VITE_API_URL;
 	import ModalDialog from '$lib/components/ModalDialog.svelte';
 	let userid = '';
 	let showDialog = false;
@@ -14,16 +15,28 @@
 
 </div>
 <ModalDialog bind:isOpen={showDialog} title="login">
-	<input type="url"
+	<label class="label" for="url">
+		<span class="label-text">OLDAP-Server:</span>
+	</label>
+	<input id="url"
+				 type="url"
 				 class="input input-bordered input-xs w-full max-w-xs"
 				 placeholder="Server-URL"
-				 bind:value={userid} />
+				 bind:value={apiUrl} />
 
-	<input type="text"
+	<label class="label" for="userid">
+		<span class="label-text">User-ID:</span>
+	</label>
+	<input id="userid"
+				 type="text"
 				 class="input input-bordered input-xs w-full max-w-xs"
 				 placeholder="User ID"
 				 bind:value={userid} />
-	<input type="password"
+	<label class="label" for="password">
+		<span class="label-text">Password:</span>
+	</label>
+	<input id="password"
+				 type="password"
 				 class="input input-bordered input-xs w-full max-w-xs"
 				 placeholder="Password" />
 </ModalDialog>
