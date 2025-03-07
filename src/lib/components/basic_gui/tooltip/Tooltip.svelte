e72a81e8f468c797cfc7312e913166788e81d394
rosenth <lukas.rosenthaler@unibas.ch>
2025-03-07 21:25:54 +0100

    more on tooltips…

--- src/lib/components/basic_gui/tooltip/Tooltip.svelte
+++ src/lib/components/basic_gui/tooltip/Tooltip.svelte
@@ -1,41 +1,43 @@
-<script>
-	import { fly } from "svelte/transition";
-	let { children, text } = $props()
-	let show = $state(false);
+<script lang="ts">
+
+	let { children, text } = $props();
+
+	let enclosingEl = $state<HTMLElement | null>(null);
+
+	let tooltipEl: HTMLElement | null = null;
+
+	function show() {
+		if (!enclosingEl) {
+			return;
+		}
+		const rect = enclosingEl.getBoundingClientRect();
+
+		tooltipEl = document.createElement('div');
+		tooltipEl.className = 'tooltip';
+		document.body.appendChild(tooltipEl);
+		tooltipEl.textContent = text;
+		Object.assign(tooltipEl?.style, {
+			position: 'absolute',
+			left: `${rect.left + window.scrollX}px`,
+			top: `${rect.bottom + window.scrollY + 8}px`,
+			background: 'black',
+			color: 'white',
+			padding: '5px 10px',
+			borderRadius: '4px',
+			fontSize: '12px',
+			whiteSpace: 'nowrap',
+			zIndex: '1000'
+		});
+	}
+
+	function hide() {
+		console.log('--->MOUSE LEFT');
+		tooltipEl?.remove();
+	}
 </script>
 
-<div class="tooltip-wrapper" role="tooltip" aria-roledescription="tooltip"
-		 onmouseenter={() => (show = true)}
-		 onmouseleave={() => (show = false)}>
-
+<div role="tooltip" bind:this={enclosingEl} onmouseenter={show} onmouseleave={hide}>
 	{@render children()}
 </div>
 
-			{#if show}
-		<div class="tooltip" transition:fly={{ y: -5, duration: 150 }}>
-			{text}
-		</div>
-			{/if}
-
-<style>
-    .tooltip-wrapper {
-        position: relative;
-        display: inline-block;
-    }
-
-    .tooltip {
-        position: absolute;
-        bottom: 120%;
-        left: 50%;
-        transform: translateX(-50%);
-        background: black;
-        color: white;
-        padding: 6px 10px;
-        border-radius: 4px;
-        font-size: 12px;
-        white-space: nowrap;
-        opacity: 0.9;
-        transition: opacity 0.2s ease-in-out;
-        z-index: 9999!important;
-    }
-</style>
+<!-- <TooltipHelper text={text} bind:open={showTooltip} anchor={buttonEl} /> -->
--- src/routes/admin/user/[userid]/+page.svelte
+++ src/routes/admin/user/[userid]/+page.svelte
@@ -16,13 +16,19 @@
 	import { AdminPermission } from '$lib/oldap/enums/admin_permissions';
 	import TableColumnTitle from '$lib/components/basic_gui/table/TableColumnTitle.svelte';
 	import Tooltip from '$lib/components/basic_gui/tooltip/Tooltip.svelte';
+	import { onMount } from 'svelte';
 
 	let { data }: PageProps = $props();
-	let authinfo = AuthInfo.fromString(sessionStorage.getItem('authinfo'));
+	let authinfo: AuthInfo;
 	let user: OldapUser | null = $state(null);
 	const ncname_pattern = /^[A-Za-z_][A-Za-z0-9._-]*$/;
 	const email_pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 	let headers: string[] = [];
+
+
+	onMount(() => {
+		authinfo = AuthInfo.fromString(sessionStorage.getItem('authinfo'));
+	});
 
 	if (data && data.userid) {
 		let config_userdata = api_config(authinfo, { userId: data.userid });
@@ -50,52 +56,82 @@
 <div class="absolute top-0 left-0 right-0 bottom-0 overflow-auto">
 	<h1>{data.userid ? m.edit() : m.add()} User "{data.userid}"</h1>
 	<form class="max-w-128">
-		<Textfield type='text' label={m.user_id()} name="userid" id="userid" placeholder="user id" required={true} value={user?.userId.toString()} pattern={ncname_pattern}/>
-		<Textfield type='text' label={m.family_name()} name="familyName" id="familyName" placeholder="family name" required={true} value={user?.familyName} />
-		<Textfield type='text' label={m.given_name()} name="givenName" id="givenName" placeholder="given name" required={true} value={user?.givenName} />
-		<Textfield type='email' label={m.email()} name="email" id="email" placeholder="john.doe@example.org" required={true} value={user?.email} pattern={email_pattern} />
-		<Togglefield label={m.is_active()} name="isActive" id="isActive"/>
+		<Textfield type='text' label={m.user_id()} name="userid" id="userid" placeholder="user id" required={true}
+							 value={user?.userId.toString()} pattern={ncname_pattern} />
+		<Textfield type='text' label={m.family_name()} name="familyName" id="familyName" placeholder="family name"
+							 required={true} value={user?.familyName} />
+		<Textfield type='text' label={m.given_name()} name="givenName" id="givenName" placeholder="given name"
+							 required={true} value={user?.givenName} />
+		<Textfield type='email' label={m.email()} name="email" id="email" placeholder="john.doe@example.org" required={true}
+							 value={user?.email} pattern={email_pattern} />
+		<Togglefield label={m.is_active()} name="isActive" id="isActive" />
 		<Table title={m.projects()} description={m.perprojperms()} action_elements={actions}>
 			<TableHeader>
 				<TableColumnTitle>{m.project()}</TableColumnTitle>
 				<TableColumnTitle> <!-- ADMIN_OLDAP -->
-					<Tooltip text="Root permissions">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-						<path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z" />
-					</svg>
+					<Tooltip text="Has root rights">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+								 stroke="currentColor" class="size-4">
+							<path stroke-linecap="round" stroke-linejoin="round"
+										d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z" />
+						</svg>
 					</Tooltip>
 				</TableColumnTitle>
 				<TableColumnTitle> <!-- ADMIN_USER -->
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-						<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-					</svg>
+					<Tooltip text="May admin users">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+								 stroke="currentColor" class="size-4">
+							<path stroke-linecap="round" stroke-linejoin="round"
+										d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+						</svg>
+					</Tooltip>
 				</TableColumnTitle>
 				<TableColumnTitle> <!-- ADMIN_PERMISSION_SETS -->
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-						<path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-						<path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-					</svg>
+					<Tooltip text="May admin permission sets">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+								 stroke="currentColor" class="size-4">
+							<path stroke-linecap="round" stroke-linejoin="round"
+										d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+							<path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+						</svg>
+					</Tooltip>
 				</TableColumnTitle>
 				<TableColumnTitle> <!-- ADMIN_RESOURCES -->
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-						<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
-					</svg>
+					<Tooltip text="May override resource permissions">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+								 stroke="currentColor" class="size-4">
+							<path stroke-linecap="round" stroke-linejoin="round"
+										d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
+						</svg>
+					</Tooltip>
 				</TableColumnTitle>
 				<TableColumnTitle> <!-- ADMIN_MODEL -->
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-						<path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
-						<path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-					</svg>
+					<Tooltip text="May modify data model">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+								 stroke="currentColor" class="size-4">
+							<path stroke-linecap="round" stroke-linejoin="round"
+										d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
+							<path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+						</svg>
+					</Tooltip>
 				</TableColumnTitle>
 				<TableColumnTitle> <!-- ADMIN_CREATE -->
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-						<path stroke-linecap="round" stroke-linejoin="round" d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 0 0 2.25-2.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v2.25A2.25 2.25 0 0 0 6 10.5Zm0 9.75h2.25A2.25 2.25 0 0 0 10.5 18v-2.25a2.25 2.25 0 0 0-2.25-2.25H6a2.25 2.25 0 0 0-2.25 2.25V18A2.25 2.25 0 0 0 6 20.25Zm9.75-9.75H18a2.25 2.25 0 0 0 2.25-2.25V6A2.25 2.25 0 0 0 18 3.75h-2.25A2.25 2.25 0 0 0 13.5 6v2.25a2.25 2.25 0 0 0 2.25 2.25Z" />
-					</svg>
+					<Tooltip text="May create new resources">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+								 stroke="currentColor" class="size-4">
+							<path stroke-linecap="round" stroke-linejoin="round"
+										d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 0 0 2.25-2.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v2.25A2.25 2.25 0 0 0 6 10.5Zm0 9.75h2.25A2.25 2.25 0 0 0 10.5 18v-2.25a2.25 2.25 0 0 0-2.25-2.25H6a2.25 2.25 0 0 0-2.25 2.25V18A2.25 2.25 0 0 0 6 20.25Zm9.75-9.75H18a2.25 2.25 0 0 0 2.25-2.25V6A2.25 2.25 0 0 0 18 3.75h-2.25A2.25 2.25 0 0 0 13.5 6v2.25a2.25 2.25 0 0 0 2.25 2.25Z" />
+						</svg>
+					</Tooltip>
 				</TableColumnTitle>
 				<TableColumnTitle> <!-- ADMIN_LISTS -->
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-						<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
-					</svg>
+					<Tooltip text="May admin thesdauri">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+								 stroke="currentColor" class="size-4">
+							<path stroke-linecap="round" stroke-linejoin="round"
+										d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+						</svg>
+					</Tooltip>
 				</TableColumnTitle>
 
 			</TableHeader>
@@ -104,25 +140,33 @@
 					<TableItem>HyperHamlet</TableItem>
 					<TableItem><input type="checkbox"></TableItem>
 					<TableItem><input type="checkbox"></TableItem>
-					<TableItem><Button round={true}>XXX</Button></TableItem>
+					<TableItem>
+						<Button round={true}>XXX</Button>
+					</TableItem>
 				</TableRow>
 				<TableRow>
 					<TableItem>SwissBritNet</TableItem>
 					<TableItem><input type="checkbox"></TableItem>
 					<TableItem><input type="checkbox"></TableItem>
-					<TableItem><Button round={true}>XXX</Button></TableItem>
+					<TableItem>
+						<Button round={true}>XXX</Button>
+					</TableItem>
 				</TableRow>
 				<TableRow>
 					<TableItem>XYZ</TableItem>
 					<TableItem><input type="checkbox"></TableItem>
 					<TableItem><input type="checkbox"></TableItem>
-					<TableItem><Button round={true}>XXX</Button></TableItem>
+					<TableItem>
+						<Button round={true}>XXX</Button>
+					</TableItem>
 				</TableRow>
 				<TableRow>
 					<TableItem>XYZ</TableItem>
 					<TableItem><input type="checkbox"></TableItem>
 					<TableItem><input type="checkbox"></TableItem>
-					<TableItem><Button round={true}>XXX</Button></TableItem>
+					<TableItem>
+						<Button round={true}>XXX</Button>
+					</TableItem>
 				</TableRow>
 			</TableBody>
 		</Table>
